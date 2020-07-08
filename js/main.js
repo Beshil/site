5829fbd0d3d19150b0802caf7a2fc87d0c934b00
Beshil <ersanbievbesh@gmail.com>
2020-07-08 10:12:59 +0300

    first thread

---
+++ js/main.js
@@ -0,0 +1,17 @@
+window.addEventListener("DOMContentLoaded", () => {
+    const tabs = document.querySelectorAll(".tabs-item");
+    tabs.forEach(elem => {
+        elem.addEventListener("click", () => {
+            tabs.forEach(otherItem => {
+                if (otherItem.classList.contains("tabs__item_active")) {
+                    otherItem.classList.remove("tabs__item_active");
+                }
+            })
+            elem.classList.add("tabs__item_active")
+        });
+    });
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 5000,
+    });
+});
